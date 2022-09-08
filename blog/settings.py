9efb03ab806624a0e87f84ce6df88dedd3e5fa85
Simeon Ndayambaje <websites17@yahoo.com>
2022-09-08 12:01:23 +0200

    message

--- blog/settings.py
+++ blog/settings.py
@@ -4,6 +4,9 @@ import environ
 from datetime import timedelta
 env = environ.Env()
 environ.Env.read_env()
+import django
+from django.utils.encoding import force_str
+django.utils.encoding.force_text = force_str
 
 ENVIRONMENT = env
 
